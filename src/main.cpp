352aafdcdde24d3a21dc0c1f4e1ad6209ac2acad
murrmaringo <twizzleannie@gmail.com>
2025-03-02 16:37:28 +0300

    shapes

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 #include <vector>
 #include <cmath>
 
+#define PI 3.1415
 
 class Field
 {
 private:
-	int size = 4;
+	int size = 10;
 	std::vector<Vector2> points;
 	int player = 0;
 	Vector2 player_pos = {0, 0};
-	Vector2 pos;
+	Vector2 target_pos;
 
 public:
 	void Update(float delta_time)
@@ -27,36 +28,42 @@ public:
 
 			if (i == player)
 			{
-				pos.x = (points[i].x + points[(i + 1) % size].x) / 2;
-				pos.y = (points[i].y + points[(i + 1) % size].y) / 2;
+				target_pos.x = (points[i].x + points[(i + 1) % size].x) / 2;
+				target_pos.y = (points[i].y + points[(i + 1) % size].y) / 2;
 			}
 		}
 
-		player_pos = Vector2Lerp(player_pos, pos, delta_time * 10);
+		player_pos = Vector2Lerp(player_pos, target_pos, delta_time * 10);
 		DrawCircleV(player_pos, 10, RED);
 	}
 
-	Field(int n) : size(n)
+	Field(int center_x, int center_y, int n) : size(n)
 	{
-		points = { {100, 100}, {400, 100}, {400, 400}, {100, 400} };
+		int point_x, point_y;
+		for (int i = 0; i < size; i++)
+		{
+			point_x = (center_x + std::sin(360 / size * i * PI / 180) * 200);
+			point_y = (center_y - std::cos(360 / size * i * PI / 180) * 200);
+			points.push_back({ point_x, point_y });
+		}
 	}
 };
 
 int main ()
 {
-	int width = 1280, height = 800;
+	int width = 1000, height = 500;
 
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-	InitWindow(width, height, "Hello Raylib");
+	InitWindow(width, height, "blizzard");
 
-	Field field = Field(4);
-	
+	Field field = Field(width / 2, height / 2, 10);
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(BLACK);
 
-		DrawText("Hello Raylib", 100, 100, 20, WHITE);
+		DrawText("SCORE: ", 10, 10, 30, WHITE);
 		field.Update(GetFrameTime());
 		
 		EndDrawing();
