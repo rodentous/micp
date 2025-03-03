7ccf661ce078bc019e91ca0e8712afb6c190552b
rodentous <therodentous@gmail.com>
2025-03-03 19:46:18 +0300

    graphics

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,14 @@
 #include <vector>
 #include <cmath>
 
-#define PI 3.1415
 
 class Field
 {
 private:
-	int size = 10;
+	int size = 10; // number of points
 	std::vector<Vector2> points;
 	std::vector<Vector2> far_points;
-	int center_x, center_y;
+	Vector2 center;
 
 	int player = 0;
 	Vector2 player_pos = {0, 0};
@@ -20,49 +19,61 @@ private:
 public:
 	void Update(float delta_time)
 	{
+		// handle input
+		if (IsKeyPressed(KEY_LEFT))
+			player = (player + 1) % size;
+		if (IsKeyPressed(KEY_RIGHT))
+			player = (player + size - 1) % size;
+		if (IsKeyPressed(KEY_SPACE))
+			projectiles.push_back(player_pos);
+		
+		// target position for player
 		Vector2 target_pos;
+		target_pos.x = (points[player].x + points[(player + 1) % size].x) / 2;
+		target_pos.y = (points[player].y + points[(player + 1) % size].y) / 2;
+
+		// draw lines
 		for (int i = 0; i < size; i++)
 		{
-			DrawLineV(points[i], points[(i + 1) % size], WHITE);
-			DrawLineV(far_points[i], far_points[(i + 1) % size], GRAY);
-			DrawLineV(points[i], far_points[i], GRAY);
-
-			if (i == player)
-			{
-				target_pos.x = (points[i].x + points[(i + 1) % size].x) / 2;
-				target_pos.y = (points[i].y + points[(i + 1) % size].y) / 2;
-			}
+			DrawLineV(points[i], points[(i + 1) % size], BLUE);
+			DrawLineV(far_points[i], far_points[(i + 1) % size], BLUE);
+			DrawLineV(points[i], far_points[i], BLUE);
 		}
 
-		if (IsKeyPressed(KEY_LEFT))
-			player = (player + size - 1) % size;
-		if (IsKeyPressed(KEY_RIGHT))
-			player = (player + 1) % size;
-		if (IsKeyPressed(KEY_SPACE))
-			projectiles.push_back(target_pos);
+		// draw lines around player
+		DrawLineV(points[player], points[(player + 1) % size], YELLOW);
+		DrawLineV(far_points[player], far_points[(player + 1) % size], YELLOW);
+		DrawLineV(points[player], far_points[player], YELLOW);
+		DrawLineV(points[(player + 1) % size], far_points[(player + 1) % size], YELLOW);
 
+		// move player
 		player_pos = Vector2Lerp(player_pos, target_pos, delta_time * 10);
+		DrawCircleV(player_pos, 10, YELLOW);
+
+		// move projectiles
 		for (Vector2 &projectile : projectiles)
 		{
-			projectile = Vector2Lerp(projectile, {center_x, center_y}, delta_time * 10);
-			if (Vector2Distance(projectile, {center_x, center_y}) > 10)
+			Vector2 offset = Vector2Add(center, (Vector2){0, 50});
+			projectile = Vector2MoveTowards(projectile, offset, delta_time * 300);
+			if (Vector2Distance(projectile, offset) > 20)
 				DrawCircleV(projectile, 5, YELLOW);
-				
 		}
-		DrawCircleV(player_pos, 10, RED);
 	}
 
-	Field(int x, int y, int n) : size(n), center_x(x), center_y(y)
+	Field(int n, Vector2 center) : size(n), center(center)
 	{
+		Vector2 offset = Vector2Add(center, (Vector2){0, 50});
+
+		// place points in regular polygon
 		for (int i = 0; i < size; i++)
 		{
-			int point_x = (center_x + std::sin(360 / size * i * PI / 180) * 200);
-			int point_y = (center_y - std::cos(360 / size * i * PI / 180) * 200);
-			points.push_back({ point_x, point_y });
+			int x = (center.x + std::sin(360 / size * i * PI / 180) * 200);
+			int y = (center.y - std::cos(360 / size * i * PI / 180) * 200);
+			points.push_back((Vector2){ x, y });
 
-			point_x = (center_x + std::sin(360 / size * i * PI / 180) * 20);
-			point_y = (center_y - std::cos(360 / size * i * PI / 180) * 20);
-			far_points.push_back({ point_x, point_y });
+			x = (offset.x + std::sin(360 / size * i * PI / 180) * 20);
+			y = (offset.y - std::cos(360 / size * i * PI / 180) * 20);
+			far_points.push_back((Vector2){ x, y });
 		}
 	}
 };
@@ -74,14 +85,14 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 	InitWindow(width, height, "blizzard");
 
-	Field field = Field(width / 2, height / 2, 10);
+	Field field = Field(10, (Vector2){width / 2, height / 2});
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(BLACK);
 
-		DrawText("SCORE: ", 10, 10, 30, WHITE);
+		DrawText("SCORE: ", 10, 10, 25, WHITE);
 		field.Update(GetFrameTime());
 		
 		EndDrawing();
